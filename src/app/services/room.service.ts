c885bbab580ec99f4d4bcb781b1215fe01e8f9c9
Pattadon Prongpreecha <pattadon.pro@ascendcorp.com>
2021-01-17 12:33:42 +0700

    add env variable

--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {RoomModel} from '../models/room.model';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class RoomService {
 
   constructor(private http: HttpClient) { }
 
-  private url = 'http://localhost:3000/api/rooms';
+  private url = environment.apiUrl + '/api/rooms';
 
   query(req: any): Observable<HttpResponse<RoomModel[]>> {
     return this.http.get<RoomModel[]>(this.url, {
